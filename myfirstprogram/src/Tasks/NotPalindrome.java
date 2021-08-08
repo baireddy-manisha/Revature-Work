d7dae29df47f8efe245d7b3c56bdfc26458e7a61
baireddy-manisha <baireddymanisha5@gmail.com>
2021-08-08 19:36:33 +0530

    tasks

---
+++ myfirstprogram/src/Tasks/NotPalindrome.java
@@ -0,0 +1,16 @@
+package Tasks;
+import java.util.Scanner;
+public class NotPalindrome {
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc=new Scanner(System.in);
+			String s=sc.nextLine();
+			String arr[]=s.split(" ");
+			for(int i=0;i<arr.length;i++) {
+			StringBuffer sb=new StringBuffer(arr[i]);
+			System.out.print(sb.reverse()+" ");
+		}
+	}
+}
+
+
